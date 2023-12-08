4a40b64fa506d6f8ff1b2951d36eadc95545c6a6
Franco Cejas <franmaticejas@gmail.com>
2023-12-08 16:40:16 -0300

    primer commit

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hola mundo</h1>
+        <img src="https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_960_720.jpg" width="400px"/> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
